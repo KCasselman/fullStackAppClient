27c98e4077d729860912b22503e34ec068dc7ca2
Kristen Casselman <kristen.casselman@yahoo.com>
2018-11-25 19:09:37 -0500

    changed signup.js

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -7,8 +7,8 @@ class Signup extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            firstname: '',
-            lastname: '',
+            firstName: '',
+            lastName: '',
             email: '',
             password: '',
         };
